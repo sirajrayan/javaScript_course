b876df6cee5deafcfc869d92807e05dc51bb4952
Siraj Khan <rayansiraj88@gmail.com>
2025-07-26 19:31:40 +0000

    Variables(const,let scop error undertand)

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=13929
+let accountEmail='sira393@gmail.com'
+accountPassword=44439
+
+// accountId=3333  error becous this is constanct 
+
+console.log(accountId)
+// the result for accountCity will be undefined
+let accountCity;
+
+accountPassword=112233
+accountEmail='dkd@gmail.com'
+console.table([accountEmail,accountPassword,accountCity])
+
+
