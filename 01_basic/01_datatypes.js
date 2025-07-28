a4aa67e5303f2ca830a627759a734cdc0277c8ac
Siraj Khan <rayansiraj88@gmail.com>
2025-07-28 06:29:05 +0000

    datatypes

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,11 @@
+
+// number => 2 to the power of 53
+let age=25
+// BigInt
+// String=""
+// Boolean =true or false
+// null=> standALone, or Object
+// undefined => undefined
+
+console.log(typeof age)
+// object --> null is object
